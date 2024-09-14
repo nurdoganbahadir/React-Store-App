4f943a8d1485033420d63733f011d86bb6dbb33b
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-14 12:40:38 +0300

    router kuruldu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Login from "./pages/Login";
+import { BrowserRouter, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
     // <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
     //   <span className="animate-bounce">🏀</span>
     //   <h1 className="animate-pulse">Welcome to Session 1 of the Clarus Store Project FS15!</h1>
     //   <span className="animate-bounce">🏀</span>
     // </div>
-    <Login />
   );
 }
 
