a499d7fac030e80b0c41b6aafdfa1d322dda42f8
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-14 11:48:34 +0300

    login başlıklar eklendi forma başlanıcak

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,10 +4,19 @@ const Login = () => {
   return (
     <div className="loginDiv">
       <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col justify-center text-center">
-        <div>
-          <span></span>
-          <h1 className="text-[22px] sm:text-[32px]">Clarus Store</h1>
+        <div className="flex justify-center items-center mt-2 gap-2">
+          <span className="w-[6px] h-[39px] bg-yellow-500"></span>
+          <h1 className="text-[22px] sm:text-[32px] font-montserrat font-[700] uppercase">
+            Clarus Store
+          </h1>
         </div>
+        <div className="my-2">
+          <h3 className="font-montserrat font-[600] text-[22px]">SIGN IN</h3>
+          <p className="font-montserrat text-labelColor text-label">
+            Enter your credentials to access your account
+          </p>
+        </div>
+        
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#FEAF00",
+        navbarColor: "#F2EAE1",
+        labelColor: "#6C6C6C",
+        white: "#fff",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        label: [
+          "14px",
+          {
+            lineHeight: "normal",
+            fontWeight: "500",
+          },
+        ],
+      },
+      boxShadow: {
+        login: "2px 5px 10px 0px rgba(0, 0, 0, 0.1)",
+      },
+    },
   },
   plugins: [],
 };
