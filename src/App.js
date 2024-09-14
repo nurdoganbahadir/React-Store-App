a56ec3860d5ff7165d0ae34a4e1f3f16ae9d953b
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-14 11:31:06 +0300

    login sayfası düzenleniyor yazı stilleri eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
-      <span className="animate-bounce">🏀</span>
-      <h1 className="animate-pulse">Welcome to Session 1 of the Clarus Store Project FS15!</h1>
-      <span className="animate-bounce">🏀</span>
-    </div>
+    // <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
+    //   <span className="animate-bounce">🏀</span>
+    //   <h1 className="animate-pulse">Welcome to Session 1 of the Clarus Store Project FS15!</h1>
+    //   <span className="animate-bounce">🏀</span>
+    // </div>
+    <Login />
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="loginDiv">
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col justify-center text-center">
+        <div>
+          <span></span>
+          <h1 className="text-[22px] sm:text-[32px]">Clarus Store</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
