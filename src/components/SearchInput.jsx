77ef45407a6499284e709ccf87d8ec27447ebbad
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-16 21:49:26 +0300

    search eklendi

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { searchIcon } from "../helper/icons";
 
-const SearchInput = () => {
+const SearchInput = ({ setSearch }) => {
   return (
-    <div>SearchInput</div>
-  )
-}
+    <div className="">
+      <div className="relative w-6/12 mx-auto">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          {searchIcon}
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block outline-none w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  focus:border-gray-900  focus:text-black"
+          placeholder="Search products..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default SearchInput
+export default SearchInput;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProductCard from "../components/ProductCard";
+import Loading from "../components/Loading";
+import SearchInput from "../components/SearchInput";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const getData = async () => {
+    setLoading(true);
     try {
       const { data } = await axios.get(
         `https://dummyjson.com/products/search?q=${search}`
@@ -14,23 +18,30 @@ const Products = () => {
       setProducts(data.products);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [search]);
 
   return (
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <SearchInput setSearch={setSearch} />
       <h2 className="text-2xl font-bold mt-8 tracking-tighter text-gray-900">
         All Products
       </h2>
-      <div className="mt-6 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 xl:gap-x-8">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="mt-6 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 xl:gap-x-8">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
